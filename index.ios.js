20e6d071721a396eae329d623467999777fa6ac1
Lucas <lucas@agtlucas.com>
2016-05-16 04:52:13 -0300

    Update ios Version

--- index.ios.js
+++ index.ios.js
@@ -3,10 +3,22 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native'
 
 class RNRC extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      color: ''
+    }
+  }
+
+  componentWillMount () {
+    this.randomColor()
+  }
 
   randomColor () {
     let color = '#' + ('000000' + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6)
@@ -16,32 +28,38 @@ class RNRC extends Component {
     return color
   }
 
+  handlerClick () {
+    this.randomColor()
+  }
+
   render () {
+    let color = this.state.color
+    console.log(color)
+
+    let containerStyle = {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: color
+    }
+
     return (
-      <View style={styles.container}>
+      <View style={containerStyle}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {color}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableHighlight onPress={this.handlerClick.bind(this)}>
+          <Text style={styles.welcome}>New Color</Text>
+        </TouchableHighlight>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
   welcome: {
+    backgroundColor: '#FFFFFF',
+    padding: 10,
     fontSize: 20,
     textAlign: 'center',
     margin: 10
